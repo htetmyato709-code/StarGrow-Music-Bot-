58fbda16081023eea42022923f5f254b1be89824
htetmyato709-code <htetmyato709@gmail.com>
2026-07-19 13:14:07 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+# ==========================
+# config.py
+# ==========================
+
+# Telegram Bot Token
+BOT_TOKEN = "8885714126:AAH-pNURgKFHoKl_Lr8OokQgHD9hsuTqbj0"
+
+# Telegram API
+API_ID = 33421644
+API_HASH = "f22e5554f51fd494a664b2cb90ec64c2"
+
+# Owner ID
+OWNER_ID = 8305397892
+
+# Database Files
+USERS_DB = "data/users.json"
+GROUPS_DB = "data/groups.json"
+
+# Command Prefix
+PLAY_COMMAND = "play"
+STOP_COMMAND = "stop"
+
+# Bot Name
+BOT_NAME = "Music Bot"
+
+# Broadcast
+ALLOW_BROADCAST = True
+
+# Mention All
+ALLOW_MENTION_ALL = True
